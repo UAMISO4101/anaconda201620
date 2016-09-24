16e6a8741c1b990e008d460edab3bc1ffb4bc72c
Henry Mora <hank8617@gmail.com>
2016-09-24 10:55:53 -0500

    Configuracion base local

--- sonidos_libres/settings.py
+++ sonidos_libres/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = 'sonidos_libres.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'SonidosLibres',
+            'HOST': 'localhost',
+            'PORT': '5432',
+            'USER': 'postgres',
+            'PASSWORD': '12345678',
+        }
     }
-}
 
 
 # Password validation
