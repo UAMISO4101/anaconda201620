f4521991be091c310b1386bc282b123236544b3a
jasmo2 <jasmo2.com@gmail.com>
2016-09-25 11:39:33 -0500

    add reducers and actions, Boilerplate

---
+++ assets/js/actions.js
@@ -0,0 +1,6 @@
+export const addNotification = name => ({ type: 'ADD_NOTIFICATION', data: name });
+export const showRequest = () => ({ type: 'SHOW_REQUEST' });
+
+export const fetchData = () => {
+  return dispatch => {}
+};
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import thunkMiddleware from 'redux-thunk';
+import * as reducers from './reducers';
+reducers.routing = routerReducer;
 
-main();
 
-function main() {
-  const app = document.getElementById("container")
-  ReactDOM.render(<App />, app);
-};
+const store = createStore(combineReducers(reducers), applyMiddleware(thunkMiddleware));
+const history = syncHistoryWithStore(browserHistory, store);
+
+import App from './components/app'
+
+function run () {
+  let state = store.getState();
+  ReactDOM.render((<Provider store={store}>
+    <Router history={history}>
+      <Route path='/' component={App}>
+      </Route>
+    </Router>
+  </Provider>), document.getElementById("container"));
+}
+
+
+
+
+function init () {
+  run();
+  store.subscribe(run);
+}
+
+init();
