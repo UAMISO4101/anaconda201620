15314986ebe34ed3009dc32d6606411342377db9
jasmo2 <jasmo2.com@gmail.com>
2016-09-25 14:55:06 -0500

    set redirection react-router

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import thunkMiddleware from 'redux-thunk';
 import * as reducers from './reducers';
@@ -10,15 +10,30 @@ reducers.routing = routerReducer;
 
 
 const store = createStore(combineReducers(reducers), applyMiddleware(thunkMiddleware));
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
 
-import App from './components/app'
+import App from './components/app';
+import Navbar from './components/navbar';
+/*
+<Router history={history}>
+  <Route path='/' component={Navbar}>
+    <Route path="dashboard" >
+      <Route path="agente-comercial" component={App} />
+    </Route>
+  </Route>
+  <Redirect from="/" to="dashboard/agente-comercial" />
+</Router>
 
+*/
 function run () {
   let state = store.getState();
   ReactDOM.render((<Provider store={store}>
     <Router history={history}>
-      <Route path='/' component={App}>
+      <Route path='/'>
+        <IndexRedirect to="/dashboard/agente-comercial" />
+        <Route path="dashboard">
+          <Route path="agente-comercial" component={App} />
+        </Route>
       </Route>
     </Router>
   </Provider>), document.getElementById("container"));
