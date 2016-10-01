374997d2b80d13c7c191a7b52df1b4e8455b4938
Henry Mora <hank8617@gmail.com>
2016-09-30 20:04:58 -0500

    Admin para SoundType y Artist

--- comercial_agent/admin.py
+++ comercial_agent/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from comercial_agent.models import Artwork, Sound
+from comercial_agent.models import Artwork, Sound, SoundType, Artist
 
 admin.site.register(Artwork)
 admin.site.register(Sound)
+admin.site.register(SoundType)
+admin.site.register(Artist)
