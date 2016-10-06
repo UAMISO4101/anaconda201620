7256af7bd62c0e6ffdd51181be8ab5345dcf22d7
jasmo2 <jasmo2.com@gmail.com>
2016-10-06 18:50:45 -0500

    refactor backend for H009

--- sonidos_libres/settings.py
+++ sonidos_libres/settings.py
@@ -84,9 +84,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'SonidosLibres',
             'HOST': 'localhost',
-            'PORT': '5432',
-            'USER': 'postgres',
-            'PASSWORD': '12345678',
+            'PORT': '5432'
         }
     }
 
