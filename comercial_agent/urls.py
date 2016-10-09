31513f1731753092d370903e2c8238548bb7bd1a
jasmo2 <jasmo2.com@gmail.com>
2016-10-08 22:34:59 -0500

    refactor to standar URLS

--- comercial_agent/urls.py
+++ comercial_agent/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^create-notification/$', views.create_notification, name='create_notification'),
-    url(r'^notificationJSON', views.notification_json, name='notificationJSON'),
-    url(r'^get-sounds/$', views.get_artworks, name='get_sounds'),
+    url(r'^notifications/$', views.notification_json, name='notificationJSON'),
+    url(r'^notifications/$', views.create_notification, name='create_notification'),
+    url(r'^notifications/(?P<notification_id>\w+)/$', views.edit_notification, name='edit_notification'),
+    url(r'^sounds/$', views.get_artworks, name='get_sounds'),
 ]
