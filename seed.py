33f77b56a0036ff5c026ccba63adbf830722e8a0
Henry Mora <hank8617@gmail.com>
2016-10-08 23:48:29 -0500

    Notificación nueva al Seed

--- seed.py
+++ seed.py
@@ -204,6 +204,29 @@ def notifications_creator():
     )
     artwork_request.save()
 
+    notification = Notification(
+        name='Not 02',
+        initial_date='2016-12-10',
+        closing_date='2017-01-11',
+        description='Not 02 Desc',
+        notification_type='PR',
+    )
+    notification.save()
+
+    artwork_request = ArtworkRequest(
+        name='AWR 04',
+        features='Features 04',
+        notification=notification,
+    )
+    artwork_request.save()
+
+    artwork_request = ArtworkRequest(
+        name='AWR 05',
+        features='Features 05',
+        notification=notification,
+    )
+    artwork_request.save()
+
 songs_creator()
 sounds_creator()
 notifications_creator()
