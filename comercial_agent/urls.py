848e5024297ec0b289e9accf3d8e69a4a40d255e
Henry Mora <hank8617@gmail.com>
2016-10-09 16:56:50 -0500

    URL para el servicio de consulta de convocatorias vigentes

--- comercial_agent/urls.py
+++ comercial_agent/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^notifications/$', views.notification_json, name='notificationJSON'),
     url(r'^notifications/$', views.create_notification, name='create_notification'),
     url(r'^notifications/(?P<notification_id>\w+)/$', views.edit_notification, name='edit_notification'),
+    url(r'^open-notifications/$', views.get_open_notifications, name='get_open_notifications'),
     url(r'^sounds/$', views.get_artworks, name='get_sounds'),
 ]
