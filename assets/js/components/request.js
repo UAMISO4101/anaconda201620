74838c439b956c72fb83de05793eb611f8efee55
jasmo2 <jasmo2.com@gmail.com>
2016-10-09 16:52:46 -0500

    refactor component

--- assets/js/components/request.js
+++ assets/js/components/request.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { ListGroupItem } from 'react-bootstrap'
+import FaEraser from 'react-icons/lib/fa/eraser'
 
-const Request = ({ rq }) => {
-  return (
-    <li className="list-group-item">
-      {`${rq.name}, ${rq.features}.`}
-    </li>);
+class Request extends Component {
+  constructor(props){
+    super(props);
+    this.deleteRequest = this.deleteRequest.bind(this);
+  }
+
+  deleteRequest(event){
+    this.props.deleteRequest(this.props.rq.id)
+  }
+
+  render(){
+    return (
+      <li className="list-group-item">
+        {`${this.props.rq.name} \n ${this.props.rq.features}.`}
+        <button type="button" onClick={this.deleteRequest} className="btn btn-default" aria-label="Left Align">
+         <FaEraser/>
+        </button>
+      </li>);
+    }
 };
 
 export default Request;
