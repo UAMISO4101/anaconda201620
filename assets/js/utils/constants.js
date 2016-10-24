341c32e6722771f5b793d0d38868b71bf12072cc
Henry Mora <hank8617@gmail.com>
2016-10-23 19:46:25 -0500

    Actualización de URL del server en constantes

--- assets/js/utils/constants.js
+++ assets/js/utils/constants.js
@@ -1,5 +1,5 @@
 export const ARTIST_DASHBOARD = '/dashboard/artista';
 export const CA_DASHBOARD = '/dashboard/agente-comercial';
-export const SERVER_URL = 'http://localhost:8000';
+export const SERVER_URL = 'https://sonidoslibres-g4-staging.herokuapp.com';
 export const SOUNDS_FILTER = {ALL:'all',RATING:'rating',RECENT:'recent'};
 export const SOUNDS_TYPE = {ALBUM:'album',SONG:'song',SOUND:'sound'};
