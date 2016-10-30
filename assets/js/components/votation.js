c42f39b4d59495b87317988da51e710d269571cb
jjcrojas <jc.rojas1@uniandes.edu.co>
2016-10-30 16:31:15 -0500

    Individual Votation Component

---
+++ assets/js/components/votation.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import { ListGroupItem } from 'react-bootstrap'
+import FaUserSecret from 'react-icons/lib/fa/user-secret'
+
+class Votations extends Component {
+  constructor(props){
+    super(props);
+    this.deleteRequest = this.deleteRequest.bind(this);
+  }
+
+  deleteRequest(event){
+    this.props.deleteRequest(this.props.rq.id)
+  }
+
+  render(){
+    let artist = this.props.vt.artist;
+    let artworks = this.props.vt.artworks;
+    return (
+      <div className="row">
+        <div className="col-sm-3" style={{textAlign: 'center'}}>
+          <FaUserSecret size={60} color='#19708D' /><br/>
+          <h4>{artist.name}</h4>
+        </div>
+        <div className="col-sm-9">
+          <div href="#" class="list-group-item list-group-item-action active">
+            <h5 class="list-group-item-heading">ABC</h5>
+            <p class="list-group-item-text">xyz</p>
+          </div>
+        </div>
+      </div>
+
+
+    );
+  }
+};
+
+export default Votations;
