4da329edbaea17cd558fd2a39302c4a283c4d845
jjcrojas <jc.rojas1@uniandes.edu.co>
2016-10-31 21:28:33 -0500

    refactor testData

--- assets/js/testData/audios.js
+++ assets/js/testData/audios.js
@@ -2,7 +2,7 @@ export const audiosDefault = [
 {
     url: 'http://a.tumblr.com/tumblr_mlyactVSyX1qejx3lo1.mp3',
     cover: 'http://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2015/06/daft-punk.jpg',
-    artist: {
+    soundtrack: {
         name: 'Daft Punk',
         song: 'Get Lucky'
     }
@@ -10,7 +10,7 @@ export const audiosDefault = [
 {
     url: 'http://a.tumblr.com/tumblr_lpoc6cHNDP1r0jthjo1.mp3',
     cover: 'http://www.thailandballoonfestival.com/tibf2013/images/HugoSlider1.jpg',
-    artist: {
+    soundtrack: {
         name: 'Hugo',
         song: '99 Problems'
     }
@@ -18,14 +18,14 @@ export const audiosDefault = [
 {
     url: 'http://claymore.france.free.fr/momo/summer love.mp3',
     cover: 'http://myuvn.com/wp-content/uploads/2015/07/justin-timberlake-pusher-love-girl.jpg',
-    artist: {
+    soundtrack: {
         name: 'Justin Timberlake',
         song: 'Summer Love'
     }
 },
 {
     url: 'http://a.tumblr.com/tumblr_lxe7hpIUPA1r3ne4ro1.mp3',
-    artist: {
+    soundtrack: {
         name: 'Michael Buble',
         song: 'Feeling Good'
     }
@@ -33,7 +33,7 @@ export const audiosDefault = [
 {
     url: 'http://dl.tak3da.com/download/1394/03/The Weeknd - Can t Feel My Face [320].mp3',
     cover: 'http://www.clickgratis.com.br/fotos-imagens/the-weekend/aHR0cDovL3d3dy5iaWxsYm9hcmQuY29tL2ZpbGVzL3N0eWxlcy9wcm9tb182NTAvcHVibGljL21lZGlhL3RoZS13ZWVrZW5kLXRoZS1oaWxscy12aWRlby1iaWxsYm9hcmQtNjUwLmpwZw==.jpg',
-    artist: {
+    soundtrack: {
         name: 'The Weekend',
         song: 'Can\'t Fell My Face'
     }
@@ -41,7 +41,7 @@ export const audiosDefault = [
 {
     url: 'http://midnightoilco.net/sitebuildercontent/sitebuilderfiles/metallicafuel.mp3',
     cover: 'http://imagens.ailhadometal.com/2015/03/Metallica3.png',
-    artist: {
+    soundtrack: {
         name: 'Metallica',
         song: 'Fuel'
     }
