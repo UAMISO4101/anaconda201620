b4a05262acce0b8b770b04b73ebb0b40700e16bf
jasmo2 <jasmo2.com@gmail.com>
2016-11-06 11:24:20 -0500

    Register And Login actions

--- assets/js/components/login.js
+++ assets/js/components/login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { ListGroupItem } from 'react-bootstrap'
+import { auth } from '../utils/auth'
 import FaUserSecret from 'react-icons/lib/fa/user-secret'
 let $divForms = null;
 let modalAnimateTime = 300;
@@ -8,9 +9,52 @@ let msgShowTime = 2000;
 class Login extends Component {
   constructor(props){
     super(props);
+    this._onLogin = this._onLogin.bind(this);
+    this._onRegister = this._onRegister.bind(this);
+    this._onSubmit = this._onSubmit.bind(this);
   }
   componentDidMount(){
     $divForms = $(this.refs.divForms);
+  }
+  _onLogin(event){ this._onSubmit(event) }
+  _onRegister(event){ this._onSubmit(event) }
+  _onSubmit(event){
+    event.preventDefault();
+    switch(event.currentTarget.id) {
+       case "login-form":
+           let username = this.refs.login_username.value; let password = this.refs.login_password.value;
+           let credentials = { username, password }
+           auth.login(credentials,(bool,res)=>{
+             if (bool) {
+               msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Login OK, redirecting...");
+                 setTimeout(() => {
+                   if(res.role == "artist"){
+                     window.location = `#/dashboard/artista/${res.id}/convocatorias`;
+                   }else {
+                     window.location = `#/dashboard/agente-comercial/convocatorias`;
+                   }
+                 }, 800);
+             } else {
+               msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Login error");
+             }
+           })
+           return false;
+           break;
+       case "register-form":
+           let $rg_username=$('#register_username').val();
+           let $rg_email=$('#register_email').val();
+           let $rg_password=$('#register_password').val();
+           if ($rg_username == "ERROR") {
+               msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Register error");
+           } else {
+               msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "success", "glyphicon-ok", "Register OK");
+           }
+           return false;
+           break;
+       default:
+           return false;
+    }
+    return false;
   }
   render(){
     return(
@@ -23,15 +67,15 @@ class Login extends Component {
         <div id="div-forms" ref="divForms" >
           <h2 className='text-center'>Registro y Login</h2>
           { /* Begin # Login Form */ }
-          <form id="login-form" ref="loginForm">
+          <form onSubmit={this._onLogin} id="login-form" ref="loginForm" noValidate>
             <div className="modal-body">
               <div id="div-login-msg">
                 <div id="icon-login-msg" className="glyphicon glyphicon-chevron-right"></div>
                 <span id="text-login-msg">Type your username and password.</span>
               </div>
-              <input id="login_username" className="form-control" type="text" placeholder="Username (type ERROR for error effect)" required />
-              <input id="login_password" className="form-control" type="password" placeholder="Password" required />
-            
+              <input ref="login_username" id="login_username" className="form-control" type="text" placeholder="Username (type ERROR for error effect)" required />
+              <input ref="login_password" id="login_password" className="form-control" type="password" placeholder="Password" required />
+
             </div>
             <div className="modal-footer">
               <div>
@@ -44,7 +88,7 @@ class Login extends Component {
           </form>
           { /* End # Login Form */ }
           { /* Begin | Register Form */ }
-          <form id="register-form" ref="registerForm" style={{display:'none'}}>
+          <form onSubmit={this._onRegister} id="register-form" ref="registerForm" style={{display:'none'}} noValidate>
             <div className="modal-body">
               <div id="div-register-msg">
                 <div id="icon-register-msg" className="glyphicon glyphicon-chevron-right"></div>
@@ -72,50 +116,9 @@ class Login extends Component {
 
 };
 
-$("form").submit(function (event) {
-   event.preventDefault();
-   switch(this.id) {
-       case "login-form":
-           let $lg_username=$('#login_username').val();
-           let $lg_password=$('#login_password').val();
-           if ($lg_username == "ERROR") {
-               msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Login error");
-           } else {
-               msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Login OK");
-           }
-           return false;
-           break;
-       case "lost-form":
-           let $ls_email=$('#lost_email').val();
-           if ($ls_email == "ERROR") {
-               msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", "Send error");
-           } else {
-               msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "success", "glyphicon-ok", "Send OK");
-           }
-           return false;
-           break;
-       case "register-form":
-           let $rg_username=$('#register_username').val();
-           let $rg_email=$('#register_email').val();
-           let $rg_password=$('#register_password').val();
-           if ($rg_username == "ERROR") {
-               msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Register error");
-           } else {
-               msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "success", "glyphicon-ok", "Register OK");
-           }
-           return false;
-           break;
-       default:
-           return false;
-   }
-   return false;
-});
-
-
-
 function msgFade ($msgId, $msgText) {
-   $msgId.fadeOut($msgAnimateTime, function() {
-       $(this).text($msgText).fadeIn($msgAnimateTime);
+   $msgId.fadeOut(msgAnimateTime, function() {
+       $(this).text($msgText).fadeIn(msgAnimateTime);
    });
 }
 
@@ -130,14 +133,14 @@ function msgChange($divTag, $iconTag, $textTag, $divClass, $iconClass, $msgText)
        $divTag.removeClass($divClass);
        $iconTag.addClass("glyphicon-chevron-right");
        $iconTag.removeClass($iconClass + " " + $divClass);
- }, $msgShowTime);
+ }, msgShowTime);
 }
 
 function modalAnimate (oldForm, newForm) {
    let $oldForm = $(oldForm);
    let $newForm = $(newForm);
    let $oldH = $oldForm.height();
-   let $newH = $newForm.height()+ 70;
+   let $newH = $newForm.height()+ 60; // needed for margin top on styles
    $divForms.css("height",$oldH);
    $oldForm.fadeToggle(modalAnimateTime, function(){
        $divForms.animate({height: $newH}, modalAnimateTime, function(){
