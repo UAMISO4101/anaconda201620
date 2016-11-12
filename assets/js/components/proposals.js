a64c0521c3590454242d79cec1a01c4ed564d6f0
jasmo2 <jasmo2.com@gmail.com>
2016-11-12 17:10:33 -0500

    add redirection when a notification is Closed
    Previous proposal has been choosen

--- assets/js/components/proposals.js
+++ assets/js/components/proposals.js
@@ -6,15 +6,39 @@ import SweetAlert from 'sweetalert-react';
 import { CA_DASHBOARD, SERVER_URL } from '../utils/constants';
 
 class Proposals extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      closingDate: null,
+      description: null,
+      initialDate: null,
+      name: null,
+      notif : null,
+      prev: false,
+    };
+  }
   componentDidMount(){
     this.props.fetchProposals(this.props.notification.id);
+    if (this.props.notification.notification_state == "CER"){
+       this.setState({show: true})
+    }
   }
 
   render(){
-    return (
-      <div >
+    if(this.props.notification.notification_state == "CER"){
+      return(
         <SweetAlert
+          show={this.state.show}
+          type="warning"
+          title="Convocatoria cerrada"
+          text="Esta Convocatoria ya está cerrada porqué se escogió la propuesta ganadora"
+          onConfirm={() => { this.setState({show: false}); window.location = `#${CA_DASHBOARD}/convocatorias`; }}
+        />
+      )
+    }else{
+      return (
+        <div >
+          <SweetAlert
             show={this.props.saModal.show}
             type={this.props.saModal.type}
             title={this.props.saModal.title}
@@ -22,15 +46,16 @@ class Proposals extends Component {
             onConfirm={() => {
               this.props.hideSAModal();
               window.location = `#${CA_DASHBOARD}/convocatorias`;
-              }
             }
-        />
+          }
+          />
         <br/><br/><br/><br/>
         <div className="list-group">
-          { this.props.proposals.map( proposal => <ProposalContent proposal={proposal} notificationId={this.props.notification.id} key={proposal.id}/> )}
+          { this.props.proposals.map( proposal => <ProposalContent proposal={proposal} notification={this.props.notification} key={proposal.id}/> )}
         </div>
       </div>
     );
+    }
   }
 };
 
