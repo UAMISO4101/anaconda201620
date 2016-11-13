601425e091b3e1d9a4d61438e74613019ff1cdc0
Henry Mora <hank8617@gmail.com>
2016-11-13 15:55:59 -0500

    Se actualiza url de los servicios de consulta y creación de convocatorias agregando el id del agente comercial

--- comercial_agent/urls.py
+++ comercial_agent/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^notifications/$', views.notification_json, name='notificationJSON'),
+    url(r'^notifications/user/(?P<user_id>\w+)/$', views.notification_json, name='notificationJSON'),
     url(r'^notifications/(?P<notification_id>\w+)/$', views.edit_notification, name='edit_notification'),
     url(r'^sounds/(?P<artwork_type>\w+)/(?P<artwork_filter>\w+)/$', views.get_artworks, name='get_sounds'),
     url(r'^notifications/open-notifications/$', views.get_open_notifications, name='get_open_notifications'),
