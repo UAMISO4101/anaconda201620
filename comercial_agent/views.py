52b7cf131a089ce6c45612be9edbeb1be8fcdbe0
Henry Mora <hank8617@gmail.com>
2016-11-19 10:20:28 -0500

    Prueba de selección de una postulación como gandora (VERDE)

--- comercial_agent/views.py
+++ comercial_agent/views.py
@@ -470,4 +470,11 @@ def get_postulations_by_notification(request,notification_id):
 
 @csrf_exempt
 def set_notification_winner(request,postulation_id):
-    return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'PUT':
+        Postulation.objects.filter(pk=postulation_id).update(
+            is_winner=True
+        )
+
+        return HttpResponse(status=status.HTTP_201_CREATED)
+    else:
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
