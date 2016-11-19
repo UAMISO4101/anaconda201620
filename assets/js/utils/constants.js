d2b7223e7031c2b834646840c11bc0e9553cf348
jasmo2 <jasmo2.com@gmail.com>
2016-11-19 12:24:18 -0500

    fix constants
    it matches the Back-End

--- assets/js/utils/constants.js
+++ assets/js/utils/constants.js
@@ -6,4 +6,4 @@ export const DEFAULT_IMAGE = 'http://www.wallcoo.net/cartoon/fiery_effect_1920_1
 export const SOUNDS_FILTER = {ALL:'all',RATING:'rating',RECENT:'recent'};
 export const SOUNDS_TYPE = {ALBUM:'album',SONG:'song',SOUND:'sound'};
 export const AUTH_TYPE = {LOGIN:'Ir a Login',REGISTER:'Ir a Registro'};
-export const USER_ROLES = {ARTIST: "artist", COMERCIAL_AGENT: "comercial_agent"}
+export const USER_ROLES = {ARTIST: "artist", COMERCIAL_AGENT: "commercial-agent"}
