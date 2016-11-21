720c6b56364089a02949aa2078dc82e58ac217cb
Daniel Ordonez <da.ordonez11@uniandes.edu.co>
2016-11-20 23:22:52 -0500

    Install Google Material UI component for react

--- assets/js/components/app.js
+++ assets/js/components/app.js
@@ -3,19 +3,23 @@ import { connect } from 'react-redux';
 import Navbar from './navbar';
 import SonidosLibresPlayerContent from '../containers/sonidosLibresPlayerContent';
 import { StickyContainer, Sticky } from 'react-sticky';
-
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import  Loading from 'react-loading';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+injectTapEventPlugin();
 const App = ({children}) => {
     return(
-        <div>
+        <MuiThemeProvider>
+          <div>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"/>
             <Navbar/>
             <div  className="navbarMarginTopForContainer back">
                 {children}
             </div>
               <SonidosLibresPlayerContent />
+              </div>
+        </MuiThemeProvider>
 
-        </div>
 
 
     )
